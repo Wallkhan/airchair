69260ade82332d535ba13bd0c2d71a9a4afe0d86
Michael Jenkin <jenkin@yorku.ca>
2023-07-22 22:49:45 -0400

    Now with the json files properly served

--- chair_ws/src/chair/chair/chair_controller.py
+++ chair_ws/src/chair/chair/chair_controller.py
@@ -1,3 +1,5 @@
+from enum import Enum
+import json
 import numpy as np
 import rclpy
 from rclpy.node import Node
@@ -7,45 +9,74 @@ from std_msgs.msg import Bool
 from rclpy.qos import DurabilityPolicy
 from rclpy.qos import HistoryPolicy
 from rclpy.qos import QoSProfile
+from geometry_msgs.msg import Twist
 from chair_interface.srv import EStop
+from chair_interface.srv import Engage
 
+class State(Enum):
+    ESTOP = "E-stop"
+    MANUAL = "Manual"
+    ENGAGED = "Engaged"
 
 class ChairController(Node):
+    """This is a class that encompasses the basic controller for the wheelchair. It is in one of the
+       states defined above. There are service calls to (try) to change from one state to another."""
+
     def __init__(self):
         super().__init__('chair_controller')
         self.get_logger().info(f'{self.get_name()} created')
 
-        self._chair_status = 'e-stop'
+        self._chair_status = State.ESTOP
         self._target_status = False
 
-        self.declare_parameter('chair_status', "/chair_a/chair_status")      # chair's current running status [e-stop, manual, engaged]
-        self.declare_parameter('target_status', "/chair_a/target_status")    # chair's view of target status [True, False] (for leader is of person)
-        self.declare_parameter('convoy_state', "/chair_a/convoy_state")      # static state of convoy [chair_a, chair_b, chair_c]
-        self.declare_parameter('chair_state', "/chair_a/chair_state")        # static state of chair Json string
-        self.declare_parameter('estop', "/chair_a/estop")                    # push or reset estop button
+        self.declare_parameter('convoy_description', "convoy.json")
+        self.declare_parameter('chair_descriptions', "chairs.json")
+        self.declare_parameter('chair_name', "chair_a")
+        self.declare_parameter('chair_status', "chair_status")      # chair's current running status (a State as defined above)
+        self.declare_parameter('target_status', "target_status")    # chair's view of target status [True, False] (for leader is of person)
+        self.declare_parameter('convoy_state', "convoy_state")      # static state of convoy Json string
+        self.declare_parameter('chair_state', "chair_state")        # static state of this chair Json string
+        self.declare_parameter('estop', "estop")                    # push or reset estop button
+        self.declare_parameter('engage', "engage")                  # if not stopped and target status, engage the chair
  
+        self.chair_name = self.get_parameter('chair_name').get_parameter_value().string_value
+
+        convoy_description = self.get_parameter('convoy_description').get_parameter_value().string_value
+        self.convoy_description = self._load_json(convoy_description)
+        chair_descriptions = self.get_parameter('chair_descriptions').get_parameter_value().string_value
+        self.chair_descriptions = self._load_json(chair_descriptions)
+   
         # latched
         chair_state = self.get_parameter('chair_state').get_parameter_value().string_value
         qos = QoSProfile(depth=1, durability=DurabilityPolicy.TRANSIENT_LOCAL, history=HistoryPolicy.KEEP_LAST)
-        self._chair_state_pub = self.create_publisher(String, chair_state, qos)
+        self._chair_state_pub = self.create_publisher(String, f"/{self.chair_name}/{chair_state}", qos)
 
         # latched
         convoy_state = self.get_parameter('convoy_state').get_parameter_value().string_value
         qos = QoSProfile(depth=1, durability=DurabilityPolicy.TRANSIENT_LOCAL, history=HistoryPolicy.KEEP_LAST)
-        self._convoy_state_pub = self.create_publisher(String, convoy_state, qos)
+        self._convoy_state_pub = self.create_publisher(String, f"/{self.chair_name}/{convoy_state}", qos)
 
         chair_status = self.get_parameter('chair_status').get_parameter_value().string_value
-        self._chair_status_pub = self.create_publisher(String, chair_status, 10)
+        self._chair_status_pub = self.create_publisher(String, f"/{self.chair_name}/{chair_status}", 10)
 
         target_status = self.get_parameter('target_status').get_parameter_value().string_value
-        self._target_status_pub = self.create_publisher(Bool, target_status, 10)
+        self._target_status_pub = self.create_publisher(Bool, f"{self.chair_name}/{target_status}", 10)
 
         # timer callback to publish status information
         self._timer = self.create_timer(0.1, self._timer_callback)
 
         estop_button = self.get_parameter('estop').get_parameter_value().string_value
-        self.create_service(EStop, estop_button, self._handle_estop)
+        self.create_service(EStop, f"/{self.chair_name}/{estop_button}", self._handle_estop)
 
+        engage_switch = self.get_parameter('engage').get_parameter_value().string_value
+        self.create_service(Engage, f"/{self.chair_name}/{engage_switch}", self._handle_engage)
+
+    def _load_json(self, file):
+        try:
+            return json.load(open(file, 'r'))
+        except Exception as e:
+            self.get_logger().info(f'{self.get_name()} unable to load/parse json file {file} {e}')
+            return None
 
     def _send_chair_state(self, text):
         self._chair_state_pub.publish(String(data=text))
@@ -54,20 +85,43 @@ class ChairController(Node):
         self._convoy_state_pub.publish(String(data=text))
 
     def _timer_callback(self):
-        self._chair_status_pub.publish(String(data=self._chair_status))
+        """Deal with publishing target and chair status information"""
+        self._chair_status_pub.publish(String(data=self._chair_status.value))
         self._target_status_pub.publish(Bool(data=self._target_status))
 
     def _handle_estop(self, request, response):
-        self.get_logger().info(f'{self.get_name()} setting estop to {request}')
+        """Request to set or reset estop"""
+        self.get_logger().info(f'{self.get_name()} setting estop to {request.estop}')
+        if request.estop:
+            self._chair_status = State.ESTOP
+        else:
+            self._chair_status = State.MANUAL
+        return response
+
+    def _handle_engage(self, request, response):
+        """Request to engage chair into convoy mode"""
+        self.get_logger().info(f'{self.get_name()} setting engage to {request.engage}')
+        if self._chair_status == SState.ESTOP:
+            response.status = False
+        elif self._chair_status == State.ENGAGED:
+            response.status = False
+        else:  # in manual mode
+            if self._target_status:
+                self._chair_status == State.ENGAGED
+                response.status = True
+            else:
+                response.status = False
         return response
 
 
 def main(args=None):
     rclpy.init(args=args)
     node = ChairController()
+    convoy_description = node.convoy_description
+    chair_description = node.chair_descriptions[node.chair_name]
     try:
-        node._send_convoy_state("Mares eat oats")
-        node._send_chair_state("Does eat oats")
+        node._send_convoy_state(json.dumps(convoy_description))
+        node._send_chair_state(json.dumps(chair_description))
         rclpy.spin(node)
     except KeyboardInterrupt:
         pass
