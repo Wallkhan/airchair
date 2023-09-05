b3f8af07a72967b98354c6d0acffa2f0e63d5eea
Walleed Khan <44369713+Wallkhan@users.noreply.github.com>
2023-09-05 10:09:10 -0400

    Added chairs and textures for 10 chairs (#15)

--- chair_ws/src/chair/chair/aruco_board.py
+++ chair_ws/src/chair/chair/aruco_board.py
@@ -9,12 +9,12 @@ aruco_dict = aruco.getPredefinedDictionary( aruco.DICT_6X6_1000 )
 aruco_params = aruco.DetectorParameters()
 
 # Name for the generated board image
-board_name = 'board.png'
+board_name = 'board_8.png'
 
 # Create Board, given the length of the markers and the separation between markers and speific ids
 markerLength = .13   # Here, our measurement unit is meters.
 markerSeparation = 0.01   # Here, our measurement unit is meters.
-ids = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8])
+ids = np.array([72, 73, 74, 75, 76, 77, 78, 79, 80])
 board = aruco.GridBoard([3,3], markerLength, markerSeparation, aruco_dict, ids)
 img = board.generateImage((1024, 1366))
 img_copy = img.copy()
@@ -27,10 +27,10 @@ frame_1 = aruco.drawDetectedMarkers(img_copy, corners_1, ids_1)
 # Print corners and ids of the detected markers
 print(f'corners_1: {len(corners_1)}, ids_1: {ids_1}, no of ids: {len(ids_1)}')
 # See the detected markers
-# cv2.imshow('GridBoard', img)
-# cv2.imshow("Detected Markers", frame_1)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+cv2.imshow('GridBoard', img)
+cv2.imshow("Detected Markers", frame_1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 # Save Board Image
 cv2.imwrite(board_name, img)
